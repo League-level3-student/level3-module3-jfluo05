77e7e46cf4bbf928fa98229349e09521953036ff
jfluo05 <jfluo05@gmail.com>
2020-03-22 12:56:17 -0700

    (confused) citites ip

--- src/_02_Cities/City.java
+++ src/_02_Cities/City.java
@@ -1,8 +1,20 @@
 package _02_Cities;
 
+import java.util.ArrayList;
+
 public abstract class City {
 	protected int population;
 	protected double growthRate;
+	
+	public static void main(String[] args) {
+		ArrayList<City> city = new ArrayList<City>();
+		city.add(new SanDiego(1407000, 1.6));
+		city.add(new Boston(673184, 1.2));
+		city.add(new LosAngeles(3976000, 1.4));
+		city.add(new Atlanta(472522, 1));
+
+		
+	}
 	
 	public City(int population, double growthRate) {
 		this.population = population;
